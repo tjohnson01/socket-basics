613760fc5987fb0fef3c07f68ef62bb905492316
Travis Johnson <tjohnson01@gmail.com>
2015-11-08 09:49:41 -0700

    Added custom message event for testing

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ var PORT = process.env.PORT || 3000,
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
+io.on('connection', function(socket){
     console.log('User connected via socket.io!');
+
+    socket.on('message', function(message){
+        console.log('Message received: ' + message.text);
+
+        // send to everybody except the person who sent it  //io.emit sends to everyone, including sender
+        socket.broadcast.emit('message', message);  
+    });
+
+    socket.emit('message', {
+        text: 'Welcome to the chat application!'
+    });
 });
 
 http.listen(PORT, function(){
